d4bf4a65fdac1bb80f11680232303143bf52eeed
Gregory Cook <gregory.cook@gmail.com>
2014-04-13 22:17:27 -0700

    add basic class for challenge

---
+++ TOChallenge/ObjectChallenge.php
@@ -0,0 +1,62 @@
+<?php
+	include_once 'MySql.php';
+	
+	class Challenge
+	{
+		var $ChallengeId;
+		var $Name;
+		var $Month;
+		var $Year;
+		var $Type;
+		var $Description;
+		var $Distance;
+		var $Time;
+		
+		function Challenge($challengeId, $name, $month, $year, $type,
+			$description, $distance, $time)
+		{
+			$this->ChallengeId = $challengeId;
+			$this->Name = $name;
+			$this->Month = $month;
+			$this->Year = $year;
+			$this->Type = $type;
+			$this->Description = $description;
+			$this->Distance = $distance;
+			$this->Time = $time;
+		}
+		
+		function Save()
+		{
+			$deleteSQL = "delete from Challenge where id=".$this->ChallengeId;
+			ExecuteStatement($deleteSQL);
+			
+			$insertSQL = "insert into Challenge(Name, Month, Year, Type, Description, Distance, Time)
+			              values('".$this->Name."',".
+			                        $this->Month.",".
+			                        $this->Year.",".
+			                        "'".$this->Type."',".
+			                        "'".$this->Description."',".
+			                        $this->Distance.",".
+			                        $this->Time.")";
+
+			ExecuteStatement($insertSQL);
+		}
+		
+	static function GetAll()
+		{
+			$selectSQL = "select * from Challenge order by Year, Month, Name";
+			$challengeRecords = GetSelectResult($selectSQL);
+			
+			$challenges = array();
+			foreach ($challengeRecords as $r)
+			{
+				$challenges[count($challenges)] = new Challenge(
+					$r['Id'], $r['Name'], $r['Month'],
+					$r['Year'], $r['Type'], $r['Description'],
+					$r['Distance'], $r['Time']);
+			}
+			
+			return $challenges;
+		}
+	}
+?>
--- TOChallenge/test.php
+++ TOChallenge/test.php
@@ -1,9 +1,19 @@
 <?php
 	include_once 'ObjectAthlete.php';
+	include_once 'ObjectChallenge.php';
 	
 	$athletes = Athlete::GetAll();
 	foreach($athletes as $athlete)
 	{
 		echo $athlete->Name."<br>";
+	}
+	
+	$challenge = new Challenge(1, "5K", 5, 2014, 'D', "A 5K", 5000, 0);
+	$challenge->Save();
+	
+	$challenges = Challenge::GetAll();
+	foreach($challenges as $c)
+	{
+		echo $c->Name;
 	}
 ?>
