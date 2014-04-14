dc2ed816b2d125ad9755b7b112fbb5cf9b1a720b
Gregory Cook <gregory.cook@gmail.com>
2014-04-13 22:22:53 -0700

    updated database with a couple of challenge/athlete rows

--- sql/TeamOarsomeChallenge.sql
+++ sql/TeamOarsomeChallenge.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 13, 2014 at 08:03 AM
+-- Generation Time: Apr 13, 2014 at 10:19 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -16,7 +16,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `TeamOarsomeChallenge`
+-- Database: `teamoarsomechallenge`
 --
 
 -- --------------------------------------------------------
@@ -32,12 +32,15 @@ CREATE TABLE `athlete` (
   `Gender` char(1) NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `athlete`
 --
 
+INSERT INTO `athlete` (`Id`, `Name`, `Gender`) VALUES
+(1, 'Gregory', 'M'),
+(2, 'Arlene', 'F');
 
 -- --------------------------------------------------------
 
@@ -72,6 +75,7 @@ CREATE TABLE `attempt` (
 DROP TABLE IF EXISTS `challenge`;
 CREATE TABLE `challenge` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
   `Month` int(11) NOT NULL,
   `Year` int(11) NOT NULL,
   `Type` enum('D','T') NOT NULL,
@@ -79,10 +83,31 @@ CREATE TABLE `challenge` (
   `Distance` int(11) DEFAULT NULL,
   `Time` decimal(6,1) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `challenge`
+--
+
+INSERT INTO `challenge` (`Id`, `Name`, `Month`, `Year`, `Type`, `Description`, `Distance`, `Time`) VALUES
+(2, '5K', 5, 2014, 'D', 'A 5K', 5000, '0.0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `current`
+--
+
+DROP TABLE IF EXISTS `current`;
+CREATE TABLE `current` (
+  `ChallengeId` int(11) NOT NULL,
+  `StartActive` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `EndActive` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`ChallengeId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `current`
 --
 
 
