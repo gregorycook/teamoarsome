ae6faca819c136d4e27bda4d33ef80d7bc1aa2fe
Gregory Cook <gregory.cook@gmail.com>
2014-04-17 17:57:54 -0700

    Load attempts for current challenge

--- TOChallenge/TOchallenge.php
+++ TOChallenge/TOchallenge.php
@@ -13,6 +13,8 @@
 	
 	$athletes = Athlete::GetAll();
 	$currentChallenge = Challenge::GetCurrent();
+	
+	$attempts = Attempt::GetForChallenge($currentChallenge->ChallengeId);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
